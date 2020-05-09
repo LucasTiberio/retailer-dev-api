d3662e3099f851ebedf53c0c44336b68789cbb89
Gabriel <gabriel-tamura@hotmail.com>
2020-05-09 17:55:32 -0300

    fixed.

--- src/knex-database.ts
+++ src/knex-database.ts
@@ -3,7 +3,8 @@ require('dotenv');
 import config from '../knexfile';
 import knexCleaner from 'knex-cleaner';
 
-console.log(process.env.NODE_ENV)
+console.log("process.env.NODE_ENV",process.env.NODE_ENV)
+console.log("config[process.env.NODE_ENV || 'test']", config[process.env.NODE_ENV || 'test'])
 
 declare var process : {
 	env: {
