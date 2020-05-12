43c8195c1513dd05b0c5dbcf816757f18358ce6b
Gabriel <gabriel-tamura@hotmail.com>
2020-05-12 11:46:31 -0300

    directives has role organization added.

--- src/graphql.ts
+++ src/graphql.ts
@@ -7,6 +7,8 @@ import { gql, IResolvers, IDirectiveResolvers } from 'apollo-server';
 import { GraphQLUpload } from 'graphql-upload';
 import jwt from 'jsonwebtoken';
 import knexDatabase from './knex-database';
+import { NextFunction } from 'express';
+import { IOrganizationRoleResponse } from './services/organization/types';
 
 declare var process : {
 	env: {
@@ -83,19 +85,17 @@ const resolversBase : IResolvers = {
 };
 
 const directiveResolvers : IDirectiveResolvers = {
-  async hasOrganizationRole(next, _, args, context): Promise<any> {
+  async hasOrganizationRole(next, _, args, context): Promise<NextFunction> {
     const userOrganizationRoles = await knexDatabase.knex('users as usr')
     .where('usr.id', context.client.id)
     .innerJoin('users_organization_roles as uor', 'usr.id', 'uor.user_id')
-    .innerJoin('organization_roles as or', 'uor.organization_id', 'or.id')
-    .select('organization_roles.name');
-    console.log("userOrganizationRoles", userOrganizationRoles)
-    // const userOrganizationRoles = ["MANAGER"] //return roles
-    const hasSpecifiedRole = userOrganizationRoles.some((role : string) => args.role.includes(role));
+    .innerJoin('organization_roles as or', 'uor.organization_role_id', 'or.id')
+    .select('or.name');
+    const hasSpecifiedRole = userOrganizationRoles.some((role: IOrganizationRoleResponse ) => args.role.includes(role.name));
     if (hasSpecifiedRole) return next();
-    throw new Error(`Must have role: ${args.role}, you have role: ${userOrganizationRoles}`)
+    throw new Error(`Must have role: ${args.role}, you have role: ${userOrganizationRoles.map((item: IOrganizationRoleResponse) => item.name)}`)
   },
-  async isAuthenticated(next, _, __, context) {
+  async isAuthenticated(next, _, __, context): Promise<NextFunction> {
     const token = context.headers['x-api-token'];
     if (!token) throw new Error("token must be provided!");
     try {
@@ -109,7 +109,7 @@ const directiveResolvers : IDirectiveResolvers = {
       throw new Error("You are not authorized.");
     }
   },
-  async isVerified(next, _, __, context) {
+  async isVerified(next, _, __, context): Promise<NextFunction> {
     const [user] = await knexDatabase.knex('users').where('id', context.client.id).select();
     if(!user.verified) throw new Error("you need verify your email!")
     return next();
