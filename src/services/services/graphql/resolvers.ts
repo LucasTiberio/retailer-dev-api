83ba54f5338da2a3dcceb94cb76ef9f70e97500e
Gabriel <gabriel-tamura@hotmail.com>
2020-05-12 11:48:29 -0300

    resolvers added.

---
+++ src/services/services/graphql/resolvers.ts
@@ -0,0 +1,19 @@
+import { IResolvers } from 'apollo-server';
+import knexDatabase from '../../../knex-database';
+import { Transaction } from 'knex';
+import service from '../service';
+import { ICreateServiceInOrganization } from '../types';
+
+const resolvers : IResolvers = {
+    Mutation: {
+        createServiceInOrganization: (_, attrs : ICreateServiceInOrganization, { client }) => {
+            const { input } = attrs;
+
+            return knexDatabase.knex.transaction((trx: Transaction) => {
+                return service.createServiceInOrganization(input.serviceId, input.organizationId, client, trx);
+            });
+        }
+      },
+};
+
+export default resolvers;
