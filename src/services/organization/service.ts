7011f137b8cb51c62dfc78254c9fb77811bb8ab8
Gabriel <gabriel-tamura@hotmail.com>
2020-05-12 09:49:52 -0300

    admin organization role attach to founder.

--- src/services/organization/service.ts
+++ src/services/organization/service.ts
@@ -1,4 +1,4 @@
-import { IOrganizationFromDB, IOrganizationPayload } from "./types";
+import { IOrganizationFromDB, IOrganizationPayload, OrganizationRoles } from "./types";
 import { IUserToken } from "../authentication/types";
 import { Transaction } from "knex";
 import database from "../../knex-database";
@@ -19,12 +19,15 @@ const createOrganization = async (createOrganizationPayload : IOrganizationPaylo
   if(!userToken) throw new Error("invalid token");
 
   try {
+
     const [organizationCreated] = await (trx || database.knex)
     .insert({
       name: createOrganizationPayload.name,
       contact_email: createOrganizationPayload.contactEmail,
       user_id: userToken.id
     }).into('organizations').returning('*')
+
+    await organizationRolesAttach(userToken.id, organizationCreated.id, OrganizationRoles.ADMIN, trx);
   
     return _organizationAdapter(organizationCreated)
   } catch(e){
@@ -38,6 +41,20 @@ const verifyOrganizationName = async (name : string, trx : Transaction) => {
   const organizationFound = await (trx || knexDatabase.knex)('organizations').where("name", name).select();
 
   return !!organizationFound.length
+
+}
+
+const organizationRolesAttach = async (userId: string, organizationId: string, roleName: OrganizationRoles, trx: Transaction) => {
+
+     const [organizationRole] = await (trx || knexDatabase.knex)('organization_roles').where('name', roleName).select('id');
+
+     if(!organizationRole) throw new Error("Organization role not found.")
+     
+     await (trx || knexDatabase.knex)('users_organization_roles').insert({
+      user_id: userId,
+      organization_id: organizationId, 
+      organization_role_id: organizationRole.id
+     })
 
 }
 
