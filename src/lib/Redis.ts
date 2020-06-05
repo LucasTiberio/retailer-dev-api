8a3426848dc8298389759248d899e745143fb0a9
Gabriel <gabriel-tamura@hotmail.com>
2020-06-05 14:46:21 -0300

    fixed.

--- src/lib/Redis.ts
+++ src/lib/Redis.ts
@@ -8,10 +8,6 @@ if(process.env.NODE_ENV === 'test'){
   
 } else {
 
-  console.log("process.env.REDIS_PORT", process.env.REDIS_CACHEKEY)
-  console.log("process.env.REDIS_PORT", process.env.REDIS_HOST)
-  console.log("process.env.REDIS_PORT", Number(process.env.REDIS_PORT))
-  
   client = redis.createClient(Number(process.env.REDIS_PORT), process.env.REDIS_HOST, {
     auth_pass: process.env.REDIS_CACHEKEY,
     tls: { servername: process.env.REDIS_HOST }
