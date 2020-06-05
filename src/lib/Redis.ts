e4705c1b8e89716d4666bfbe8699ada8f1e434d4
Gabriel <gabriel-tamura@hotmail.com>
2020-06-05 14:40:41 -0300

    consoles added.

--- src/lib/Redis.ts
+++ src/lib/Redis.ts
@@ -7,6 +7,10 @@ if(process.env.NODE_ENV === 'test'){
   client = redis.createClient(Number(process.env.REDIS_PORT), process.env.REDIS_HOST);
   
 } else {
+
+  console.log("process.env.REDIS_PORT", process.env.REDIS_CACHEKEY)
+  console.log("process.env.REDIS_PORT", process.env.REDIS_HOST)
+  console.log("process.env.REDIS_PORT", Number(process.env.REDIS_PORT))
   
   client = redis.createClient(Number(process.env.REDIS_PORT), process.env.REDIS_HOST, {
     auth_pass: process.env.REDIS_CACHEKEY,
