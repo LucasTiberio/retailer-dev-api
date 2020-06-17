2448f8c61d0c5e0c6f0e4b3bd70b5c3a90c7b1b8
Gabriel <gabriel-tamura@hotmail.com>
2020-06-17 16:17:10 -0300

    try catch shorter url

--- src/app.ts
+++ src/app.ts
@@ -18,21 +18,28 @@ app.get('/:shortenCode', async (req, res, next) => {
 
   const shortenCode = req.params.shortenCode;
 
-  const [shortIdFoundOnDb] = await knexDatabase.knex('url_shorten')
-  .where('url_code', shortenCode)
-  .select('original_url', 'id');
+  try {
+    
+    const [shortIdFoundOnDb] = await knexDatabase.knex('url_shorten')
+    .where('url_code', shortenCode)
+    .select('original_url', 'id');
+  
+    if(!shortIdFoundOnDb) throw new Error('Shortener url doesnt exists');
+  
+    await knexDatabase.knex('url_shorten')
+    .where('id', shortIdFoundOnDb.id)
+    .increment('count', 1)
+  
+    let hasPrefix = shortIdFoundOnDb.original_url.match(/https?:\/\//ig);
+  
+    res.redirect(301, hasPrefix.length ? shortIdFoundOnDb.original_url : `https://${shortIdFoundOnDb.original_url}`);
+    res.end();
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 
-  if(!shortIdFoundOnDb) throw new Error("Shortener url doesnt exists");
 
-  await knexDatabase.knex('url_shorten')
-  .where('id', shortIdFoundOnDb.id)
-  .increment('count', 1)
-
-  let hasPrefix = shortIdFoundOnDb.original_url.match(/https?:\/\//ig);
-
-  res.redirect(301, hasPrefix.length ? shortIdFoundOnDb.original_url : `https://${shortIdFoundOnDb.original_url}`);
-
-  res.end();
+  
 });
 
 server.applyMiddleware({app, cors: true});
