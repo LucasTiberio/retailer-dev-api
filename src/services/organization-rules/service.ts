cffd0f00d502d57153eabedb1548119a1adcd42f
Gabriel <gabriel-tamura@hotmail.com>
2020-07-11 20:25:16 -0300

    feat: build error fixed.

--- src/services/organization-rules/service.ts
+++ src/services/organization-rules/service.ts
@@ -5,12 +5,18 @@ const getAffiliateTeammateRules = async (organizationId: string) => {
     const query = `
     query getCurrentOrganizationPlan($input: GetCurrentOrganizationPlanInput!) {
         getCurrentOrganizationPlan(input: $input){
-            affiliateRules{
-                maxAnalysts
-                maxSales
-                maxTeammates
-                maxTransactionTax
+          planRules{
+            serviceName
+            rules{
+              maxTransactionTax
+              maxTeammates
+              maxAnalysts
+              maxSales
+              support
+              training
+              sso
             }
+          }
         }
     }`
 
@@ -28,7 +34,7 @@ const getAffiliateTeammateRules = async (organizationId: string) => {
       throw new Error(res.data.errors[0].message)
     }
 
-    return res.data.data.getCurrentOrganizationPlan
+    return res.data.data.getCurrentOrganizationPlan.planRules[0].rules
 
   } catch (error) {
       console.log(error.response.data)
