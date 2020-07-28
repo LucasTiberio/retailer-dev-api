94fe52b2fad637c966b0782bb879c345f06a37e1
Gabriel <gabriel-tamura@hotmail.com>
2020-07-28 18:51:01 -0300

    feat: payment subscription return nullable

--- src/services/payments/service.ts
+++ src/services/payments/service.ts
@@ -1,193 +1,185 @@
-import { ICreateSubscribe, PaymentMethod, ISaveCreditCard, ICreditCard, Billing, Customer } from './types';
-import { paymentFactory, fetchPaymentsService, updatePaymentsFactory } from './helpers';
-import OrganizationService from '../organization/service';
-import knexDatabase from '../../knex-database';
+import {
+  ICreateSubscribe,
+  PaymentMethod,
+  ISaveCreditCard,
+  ICreditCard,
+  Billing,
+  Customer,
+} from "./types";
+import {
+  paymentFactory,
+  fetchPaymentsService,
+  updatePaymentsFactory,
+} from "./helpers";
+import OrganizationService from "../organization/service";
+import knexDatabase from "../../knex-database";
 
 const createOrganizationCustomerPayment = async (
-    createSubscribeInput: {
-      token: string
-    },
-    context: { organizationId: string }
+  createSubscribeInput: {
+    token: string;
+  },
+  context: { organizationId: string }
 ) => {
-
   const query = `
     mutation createOrganizationCustomerPayment($input: CreateOrganizationCustomerPaymentInput!){
       createOrganizationCustomerPayment(input: $input)
-    }`
+    }`;
 
   const variables = {
     input: {
       organizationId: context.organizationId,
-      token: createSubscribeInput.token
-    }
+      token: createSubscribeInput.token,
+    },
   };
 
   try {
-
     const res = await fetchPaymentsService(query, variables);
 
-    if(res.data?.errors){
-      throw new Error(res.data.errors[0].message)
+    if (res.data?.errors) {
+      throw new Error(res.data.errors[0].message);
     }
-  
-    return res.data.data.createOrganizationCustomerPayment
-  
+
+    return res.data.data.createOrganizationCustomerPayment;
   } catch (error) {
     throw new Error(error.message);
   }
-  
-}
+};
 
-const createOrganizationCustomer = async (input: {
-  cpfCnpj: string
-  number: string
-  zipCode: string
-  complement?: string
-}, context: {organizationId: string}) => {
+const createOrganizationCustomer = async (
+  input: {
+    cpfCnpj: string;
+    number: string;
+    zipCode: string;
+    complement?: string;
+  },
+  context: { organizationId: string }
+) => {
+  const { cpfCnpj, number, zipCode, complement } = input;
 
-  const { 
-    cpfCnpj,
-    number,
-    zipCode,
-    complement
-  } = input;
+  const { organizationId } = context;
 
-  const { 
+  const organization = await OrganizationService.getOrganizationById(
     organizationId
-  } = context;
-
-  const organization = await OrganizationService.getOrganizationById(organizationId);
+  );
 
   const query = `
     mutation createOrganizationCustomer($input: CreateOrganizationCustomerInput!) {
         createOrganizationCustomer(input: $input)
-    }`
+    }`;
 
-  let variables : any = {
-      input: {
-        name: organization.name,
-        email: organization.contactEmail,
-        cpfCnpj,
-        number,
-        zipCode,
-        organizationId
-      }
+  let variables: any = {
+    input: {
+      name: organization.name,
+      email: organization.contactEmail,
+      cpfCnpj,
+      number,
+      zipCode,
+      organizationId,
+    },
   };
 
-  if(complement){
+  if (complement) {
     variables.input.complement = complement;
   }
 
   try {
-
     const res = await fetchPaymentsService(query, variables);
 
-    if(res.data?.errors){
-      throw new Error(res.data.errors[0].message)
+    if (res.data?.errors) {
+      throw new Error(res.data.errors[0].message);
     }
-  
-    return res.data.data.createOrganizationCustomer
-  
-  } catch (error) {    
+
+    return res.data.data.createOrganizationCustomer;
+  } catch (error) {
     throw new Error(error.message);
   }
+};
 
-}
+const editOrganizationCustomer = async (
+  input: {
+    cpfCnpj: string;
+    number: string;
+    zipCode: string;
+    complement?: string;
+  },
+  context: { organizationId: string }
+) => {
+  const { cpfCnpj, number, zipCode, complement } = input;
 
-const editOrganizationCustomer = async (input: {
-  cpfCnpj: string
-  number: string
-  zipCode: string
-  complement?: string
-}, context: {organizationId: string}) => {
-
-  const { 
-    cpfCnpj,
-    number,
-    zipCode,
-    complement
-  } = input;
-
-  const { 
-    organizationId
-  } = context;
+  const { organizationId } = context;
 
   const query = `
     mutation editOrganizationCustomer($input: EditOrganizationCustomerInput!) {
         editOrganizationCustomer(input: $input)
-    }`
+    }`;
 
-  let variables : any = {
-      input: {
-        cpfCnpj,
-        number,
-        zipCode,
-        organizationId
-      }
+  let variables: any = {
+    input: {
+      cpfCnpj,
+      number,
+      zipCode,
+      organizationId,
+    },
   };
 
-  if(complement){
+  if (complement) {
     variables.input.complement = complement;
   }
 
   try {
-
     const res = await fetchPaymentsService(query, variables);
 
-    if(res.data?.errors){
-      throw new Error(res.data.errors[0].message)
+    if (res.data?.errors) {
+      throw new Error(res.data.errors[0].message);
     }
-  
-    return res.data.data.editOrganizationCustomer
-  
-  } catch (error) {    
+
+    return res.data.data.editOrganizationCustomer;
+  } catch (error) {
     throw new Error(error.message);
   }
+};
 
-}
-
-const sendRecurrencyTransaction = async (input: {
-  planIdentifier: string
-  payableWith: PaymentMethod
-}, context: {organizationId: string}) => {
-
+const sendRecurrencyTransaction = async (
+  input: {
+    planIdentifier: string;
+    payableWith: PaymentMethod;
+  },
+  context: { organizationId: string }
+) => {
   const query = `
     mutation sendRecurrencyTransaction($input: SendRecurrencyTransactionInput!) {
         sendRecurrencyTransaction(input: $input)
-    }`
+    }`;
 
   const variables = {
-      input: {
-        ...input,
-        organizationId: context.organizationId
-      }
+    input: {
+      ...input,
+      organizationId: context.organizationId,
+    },
   };
 
   try {
-
     const res = await fetchPaymentsService(query, variables);
 
-    if(res.data?.errors){
-      throw new Error(res.data.errors[0].message)
+    if (res.data?.errors) {
+      throw new Error(res.data.errors[0].message);
     }
 
-    await knexDatabase.knex('organizations')
-      .where('id', context.organizationId)
+    await knexDatabase
+      .knex("organizations")
+      .where("id", context.organizationId)
       .update({
         free_trial: false,
-        free_trial_expires: null
-      })
-  
-    return res.data.data.sendRecurrencyTransaction
-  
-  } catch (error) {    
+        free_trial_expires: null,
+      });
+
+    return res.data.data.sendRecurrencyTransaction;
+  } catch (error) {
     throw new Error(error.message);
   }
-
-}
+};
 
 const listAvailablePlans = async () => {
-
   const query = `
   query listAvailablePlans{
     listAvailablePlans{
@@ -211,27 +203,23 @@ const listAvailablePlans = async () => {
         }
       }
     }
-  }`
+  }`;
 
   try {
-
     const res = await fetchPaymentsService(query);
 
-    if(res.data?.errors){
-      throw new Error(res.data.errors[0].message)
+    if (res.data?.errors) {
+      throw new Error(res.data.errors[0].message);
     }
 
-    return res.data.data.listAvailablePlans
-  
+    return res.data.data.listAvailablePlans;
   } catch (error) {
-    console.log(error.response.data)
+    console.log(error.response.data);
     throw new Error(error.message);
   }
-
-}
+};
 
 const listOrganizationCustomerPayment = async (organizationId: string) => {
-
   const query = `
     query listOrganizationCustomerPayment($input: ListOrganizationCustomerPaymentInput!){
       listOrganizationCustomerPayment(input: $input){
@@ -245,31 +233,28 @@ const listOrganizationCustomerPayment = async (organizationId: string) => {
         }
       }
     }
-  `
+  `;
 
   const variables = {
-      input: {
-        organizationId
-      }
+    input: {
+      organizationId,
+    },
   };
 
   try {
-
     const res = await fetchPaymentsService(query, variables);
 
-    if(res.data?.errors){
-      throw new Error(res.data.errors[0].message)
+    if (res.data?.errors) {
+      throw new Error(res.data.errors[0].message);
     }
 
-    return res.data.data.listOrganizationCustomerPayment
-
+    return res.data.data.listOrganizationCustomerPayment;
   } catch (error) {
     throw new Error(error.message);
   }
-}
+};
 
 const getSubscriptionByOrganizationId = async (organizationId: string) => {
-
   const query = `
     query getSubscriptionByOrganizationId($input: GetSubscriptionByOrganizationIdInput!) {
         getSubscriptionByOrganizationId(input: $input){
@@ -313,35 +298,34 @@ const getSubscriptionByOrganizationId = async (organizationId: string) => {
             secureUrl
           }
         }
-    }`
+    }`;
 
   const variables = {
-      input: {
-        organizationId
-      }
+    input: {
+      organizationId,
+    },
   };
 
   try {
-
     const res = await fetchPaymentsService(query, variables);
 
-    if(res.data?.errors){
-      throw new Error(res.data.errors[0].message)
+    if (res.data?.errors) {
+      return null;
     }
 
-    return res.data.data.getSubscriptionByOrganizationId
-
+    return res.data.data.getSubscriptionByOrganizationId;
   } catch (error) {
     throw new Error(error.message);
   }
+};
 
-}
-
-const updateRecurrencyTransaction = async (input: {
-  planIdentifier: string
-  payableWith: PaymentMethod
-},organizationId: string) => {
-
+const updateRecurrencyTransaction = async (
+  input: {
+    planIdentifier: string;
+    payableWith: PaymentMethod;
+  },
+  organizationId: string
+) => {
   const query = `
     mutation updateRecurrencyTransaction($input: UpdateRecurrencyTransactionInput!) {
         updateRecurrencyTransaction(input: $input){
@@ -351,33 +335,29 @@ const updateRecurrencyTransaction = async (input: {
           priceCents
           payableWith
         }
-    }`
+    }`;
 
   const variables = {
-      input: {
-        ...input,
-        organizationId
-      }
+    input: {
+      ...input,
+      organizationId,
+    },
   };
 
   try {
-
     const res = await fetchPaymentsService(query, variables);
 
-    if(res.data?.errors){
-      throw new Error(res.data.errors[0].message)
+    if (res.data?.errors) {
+      throw new Error(res.data.errors[0].message);
     }
 
-    return res.data.data.updateRecurrencyTransaction
-
+    return res.data.data.updateRecurrencyTransaction;
   } catch (error) {
     throw new Error(error.message);
   }
-
-}
+};
 
 const cancelRecurrencyTransaction = async (organizationId: string) => {
-
   const query = `
     mutation cancelRecurrencyTransaction($input: CancelRecurrencyTransactionInput!) {
         cancelRecurrencyTransaction(input: $input){
@@ -387,32 +367,28 @@ const cancelRecurrencyTransaction = async (organizationId: string) => {
           priceCents
           payableWith
         }
-    }`
+    }`;
 
   const variables = {
-      input: {
-        organizationId
-      }
+    input: {
+      organizationId,
+    },
   };
 
   try {
-
     const res = await fetchPaymentsService(query, variables);
 
-    if(res.data?.errors){
-      throw new Error(res.data.errors[0].message)
+    if (res.data?.errors) {
+      throw new Error(res.data.errors[0].message);
     }
 
-    return res.data.data.cancelRecurrencyTransaction
-
+    return res.data.data.cancelRecurrencyTransaction;
   } catch (error) {
     throw new Error(error.message);
   }
-
-}
+};
 
 const activateRecurrencyTransaction = async (organizationId: string) => {
-
   const query = `
     mutation activateRecurrencyTransaction($input: ActivateRecurrencyTransactionInput!) {
         activateRecurrencyTransaction(input: $input){
@@ -422,64 +398,57 @@ const activateRecurrencyTransaction = async (organizationId: string) => {
           priceCents
           payableWith
         }
-    }`
+    }`;
 
   const variables = {
-      input: {
-        organizationId
-      }
+    input: {
+      organizationId,
+    },
   };
 
   try {
-
     const res = await fetchPaymentsService(query, variables);
 
-    if(res.data?.errors){
-      throw new Error(res.data.errors[0].message)
+    if (res.data?.errors) {
+      throw new Error(res.data.errors[0].message);
     }
 
-    return res.data.data.activateRecurrencyTransaction
-
+    return res.data.data.activateRecurrencyTransaction;
   } catch (error) {
     throw new Error(error.message);
   }
-
-}
+};
 
 const removeOrganizationCustomerPayment = async (
   input: {
-    paymentId: string
+    paymentId: string;
   },
   context: { organizationId: string }
 ) => {
-
   const query = `
     mutation removeOrganizationCustomerPayment($input: RemoveOrganizationCustomerPaymentInput!) {
         removeOrganizationCustomerPayment(input: $input)
-    }`
+    }`;
 
   const variables = {
-      input: {
-        ...input,
-        organizationId: context.organizationId
-      }
+    input: {
+      ...input,
+      organizationId: context.organizationId,
+    },
   };
 
   try {
-
     const res = await fetchPaymentsService(query, variables);
 
-    if(res.data?.errors){
-      throw new Error(res.data.errors[0].message)
+    if (res.data?.errors) {
+      throw new Error(res.data.errors[0].message);
     }
 
-    return res.data.data.removeOrganizationCustomerPayment
-
+    return res.data.data.removeOrganizationCustomerPayment;
   } catch (error) {
     throw new Error(error.message);
   }
-
-}
+};
 
 export default {
   createOrganizationCustomerPayment,
@@ -492,5 +461,5 @@ export default {
   listAvailablePlans,
   listOrganizationCustomerPayment,
   cancelRecurrencyTransaction,
-  editOrganizationCustomer
-}
+  editOrganizationCustomer,
+};
