494676fbd7ce10f6c73b28325aeafd5c54e457e3
Gabriel <gabriel-tamura@hotmail.com>
2020-08-23 16:54:14 -0300

    fix: connections pools fixed.

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,16 @@
 require('dotenv')
 
-const user = process.env.DB_USER;
-const password = process.env.DB_PASSWORD;
-const database = process.env.DB_DATABASE;
-const host = process.env.DB_HOST;
-const port = process.env.DB_PORT;
+const user = process.env.DB_USER
+const password = process.env.DB_PASSWORD
+const database = process.env.DB_DATABASE
+const host = process.env.DB_HOST
+const port = process.env.DB_PORT
 
-const testUser = process.env.DB_TEST_USER;
-const testPassword = process.env.DB_TEST_PASSWORD;
-const testDatabase = process.env.DB_TEST_DATABASE;
-const testHost = process.env.DB_TEST_HOST;
-const testPort = process.env.DB_TEST_PORT;
+const testUser = process.env.DB_TEST_USER
+const testPassword = process.env.DB_TEST_PASSWORD
+const testDatabase = process.env.DB_TEST_DATABASE
+const testHost = process.env.DB_TEST_HOST
+const testPort = process.env.DB_TEST_PORT
 
 const config = {
   client: 'pg',
@@ -25,22 +25,21 @@ const config = {
   requestTimeout: 10000,
   connectionTimeout: 10000,
   migrations: {
-    directory: __dirname + '/migrations'
+    directory: __dirname + '/migrations',
   },
   seeds: {
-    directory: __dirname + '/seeds'
+    directory: __dirname + '/seeds',
   },
   pool: {
-    max: 10,
+    max: 47,
     min: 1,
-    afterCreate: function(connection, callback) {
-      connection.query("SET TIME ZONE 'UTC'", function(err) {
-        callback(err, connection);
-      });
+    afterCreate: function (connection, callback) {
+      connection.query("SET TIME ZONE 'UTC'", function (err) {
+        callback(err, connection)
+      })
     },
   },
-
-};
+}
 
 const testConfig = {
   client: 'pg',
@@ -53,7 +52,7 @@ const testConfig = {
     ssl: false,
   },
   seeds: {
-    directory: __dirname + '/seeds/test'
+    directory: __dirname + '/seeds/test',
   },
   debug: false,
   requestTimeout: 5000,
@@ -62,22 +61,22 @@ const testConfig = {
   pool: {
     max: 22,
     min: 1,
-    afterCreate: function(connection, callback) {
-      connection.query("SET TIME ZONE 'UTC'", function(err) {
-        callback(err, connection);
-      });
+    afterCreate: function (connection, callback) {
+      connection.query("SET TIME ZONE 'UTC'", function (err) {
+        callback(err, connection)
+      })
     },
   },
-};
+}
 
-const onUpdateTrigger = table => `
+const onUpdateTrigger = (table) => `
     CREATE TRIGGER ${table}_updated_at
     BEFORE UPDATE ON ${table}
     FOR EACH ROW
     EXECUTE PROCEDURE on_update_timestamp();
-`;
+`
 
 module.exports.test = testConfig
 module.exports.development = config
 module.exports.production = config
-module.exports.onUpdateTrigger = onUpdateTrigger;
+module.exports.onUpdateTrigger = onUpdateTrigger
