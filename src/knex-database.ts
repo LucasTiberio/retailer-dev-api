6fda0d202102db323208c3f6f0bd47e29ea15290
Vinicius Alves <viniciuseduardo.rj@gmail.com>
2020-10-22 23:25:18 -0300

    feat: create metrics status database connect

--- src/knex-database.ts
+++ src/knex-database.ts
@@ -5,6 +5,9 @@ import knexCleaner from 'knex-cleaner';
 
 const logger = require('pino')();
 
+const promClient = require('prom-client');
+const dbPgUp = new promClient.Gauge({name: 'pg_connect_up', help: 'PostgreSQL Connected help'});
+
 let knexConfig = knex(config);
 let knexConfigTest = knex(configTest);
 
@@ -23,6 +26,7 @@ export async function connectDB(retries = 5) {
 		try {
 			await knexConfig.raw("select 1 as result")
 			.then((resp) => {
+				dbPgUp.set(1);
 				logger.info(`Successfully connected to PostgreSQL`);
 			});
 			break;
@@ -30,10 +34,15 @@ export async function connectDB(retries = 5) {
 			logger.error(`Couldn't connect to DB: ${error}`);
 
 			retries -= 1
-			logger.info(`retries left: ${retries}`)
+			logger.info(`retries left: ${retries}`);
 
+			if( retries == 0 ) {
+				dbPgUp.set(0);
+				logger.error(`Unsuccessfully connecting to PostgreSQL`);
+				return process.exit(1);
+			}
 			// wait 5 seconds
-			await new Promise(res => setTimeout(res, 5000))
+			await new Promise(res => setTimeout(res, 5000));
 		}
 	}
 }
