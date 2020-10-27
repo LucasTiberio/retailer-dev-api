4d94388b3b0662a72f82b57c99389fac5dd9a812
Renato D'Arrigo <renato.darrigo@plugone.io>
2020-10-27 19:37:42 -0300

    downloadFinantialConciliationCSVByReferenceMonth

--- src/services/finantial-conciliation/graphql/resolvers.ts
+++ src/services/finantial-conciliation/graphql/resolvers.ts
@@ -24,6 +24,17 @@ const resolvers: IResolvers = {
         return service.getOrderListByAffiliateIdAndReferenceMonth({ organizationId, affiliateId, referenceMonth }, trx)
       })
     },
+    downloadFinantialConciliationCSVByReferenceMonth: async (_, { input: { referenceMonth } }, { organizationId }) => {
+      return database.knexConfig.transaction(async (trx: Transaction) => {
+        const affiliateValues = await service.getAffiliatesValuesByMonth({ organizationId, year_month: referenceMonth }, trx)
+        let returnCSV = 'Membro;CPF;Banco;Agência;Conta;Total em vendas;Comissão\n'
+        affiliateValues.affiliates.forEach((affiliateValue) => {
+          returnCSV +=
+            `${affiliateValue.name};${affiliateValue.document};${affiliateValue.bank};${affiliateValue.agency};${affiliateValue.account};${affiliateValue.revenue};${affiliateValue.commission}` + '\n'
+        })
+        return Buffer.from(returnCSV).toString('base64')
+      })
+    },
   },
   Mutation: {
     handleOrganizationFinantialConciliationConfiguration: (_, { input }, { organizationId }) => {
